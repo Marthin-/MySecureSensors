628f40468a20b4289322f3f2a64930289157c2e6
Marthin- <martin.guilloux@gmail.com>
2016-07-01 13:34:47 +0200

    Saving before test&destroy...

--- init/init.ino
+++ init/init.ino
@@ -7,7 +7,7 @@
 
 AES aes;
 unsigned long long int IV;
-byte to_send[40];
+byte buff[40];
 byte nodeID; //A modifier plus tard
 File myFile;
 byte AES_256_key[32];
@@ -59,10 +59,10 @@ void setup() {
   Serial.println("Sending IV and AES key through I2C");
     Wire.beginTransmission(8);
     Serial.print("Sent buffer : ");
-    memcpy(to_send,(byte*)IV,8);
-    memcpy(to_send+8,AES_256_key,32);
-    Serial.write(to_send,40);
-    Wire.write(to_send,40);
+    memcpy(buff,(byte*)IV,8);
+    memcpy(buff+8,AES_256_key,32);
+    Serial.write(buff,40);
+    Wire.write(buff,40);
     Wire.endTransmission();
     Serial.println("\n[OK]\n");
 
@@ -109,10 +109,27 @@ void setup() {
   Wire.begin(8);
   EEPROM.get(2,nodeID);
   Wire.onRequest(sendNodeId);
+  Serial.print("Sending (parent) node ID to gateway : ");
+  Serial.println(nodeID);
+  Serial.println("[OK]\n");
   
 /******** Receive AES IV & key, store it into EEPROM *******/
 
   Wire.onReceive(receiveAes);
+  byte buff_8[8];
+  for(int i=0;i<8;i++){
+    buff_8[i]=buff[i];
+  }
+  for(int i=8;i<40;i++){
+    AES_256_key[i-8]=buff[i];
+  }
+  EEPROM.put(396,AES_256_key);  //396 = EEPROM_RF_ENCRYPTION_AES_KEY_ADDRESS
+  EEPROM.put(429,buff_8);       //429 = EEPROM_RF_ENCRYPTION_AES_IV_ADDRESS
+  Serial.print("Received and stored (not human-readable) AES 256 bit key :\n\"");
+  Serial.write(AES_256_key,32);
+  Serial.println("\"\nReceived and stored (not human-readable) AES initialization vector :\n\"");
+  Serial.write(buff_8,8);
+  Serial.println("\"\n[OK]\n");
   
   
   #endif
@@ -130,6 +147,10 @@ void sendNodeId(){
 
 void receiveAes(){
   //TODO
-  //byte received_IV[8];
-  //memcpy()
+  byte i=0;
+  /* store received byte array in buff (reduce, recycle, reuse ;) */
+  while(Wire.available()){
+    buff[i]=Wire.read();
+    i++;
+  }
 }
